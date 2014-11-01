b0f930826aeb904be18a111c1859acfbc50da9a5
Zelly Elly <zellyellybear@gmail.com>
2014-11-01 18:55:10 -0500

    version 3
    3 Added ability to print to chat when voice chat happens

--- ZVoiceChatDebug.lua
+++ ZVoiceChatDebug.lua
@@ -5,10 +5,15 @@
 -- https://github.com/Zelly/ZellyLuas for latest version
 -- Log saves to fs_homepath/fs_game/voicechat.log
 -- You can have it stream a log or just have it save the entire log at the end of the round
--- version: 2
+-- version: 3
+-- VERSION HISTORY:
+-- 3 Added ability to print to chat when voice chat happens
+-- 2 Fixed some errors
+-- 1 Intial release
 
-local _writePath    = string.gsub(et.trap_Cvar_Get("fs_homepath") .. "/" .. et.trap_Cvar_Get("fs_game") .. "/voicechat.log","\\","/")
-local _StreamFile = true -- If you want it to save after each new log, then set this true, false saves at end of round
+local _writePath  = string.gsub(et.trap_Cvar_Get("fs_homepath") .. "/" .. et.trap_Cvar_Get("fs_game") .. "/voicechat.log","\\","/")
+local _StreamFile = true    -- If you want it to save after each new log, then set this true, false saves at end of round
+local _PrintChat  = false   -- Set to true if you want to debug while your ingame, probably more useful now that I think about it
 
 local LogFile = { }
 
@@ -25,6 +30,9 @@ end
 local _Log = function(message)
     et.G_LogPrint( "VSAY_CHECK: " .. message .. "\n")
     LogFile[#LogFile+1] = os.date("[%X]") .. " " .. message .. "\n"
+    if ( _PrintChat  ) then
+        et.trap_SendServerCommand (clientNum, "chat \"^ovcdebug: ^7"..message.."\"")
+    end
     if ( _StreamFile ) then
         _Save()
     end
